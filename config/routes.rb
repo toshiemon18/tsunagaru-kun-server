45b003ee01e21357cf4e310a3006a5718eb1f71d
Toshiaki Seino <st12318@gmail.com>
2018-07-09 17:34:42 +0900

    すべてをv1に収めた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :devices
-  resources :users
+  devise_for :users, only: []
   namespace :v1, defaults: {format: :json} do
     resource :login, only: [:create], controller: :session
+    resources :devices
+    resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
