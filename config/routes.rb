edecc589b92d719577fafd16e791b68ef55c27ea
Toshiaki Seino <st12318@gmail.com>
2018-07-13 20:26:40 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, only: []
   namespace :v1, defaults: {format: :json} do
-    resource :login, only: [:create], controller: :session
+    resource :login, only: [:create], controller: :sessions
     resources :devices
-    resources :users
+    resources :users, only: [:create]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
