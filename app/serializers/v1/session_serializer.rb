5cdc291eb91ab55bcddbc2f1ac5cff474c8f6e47
Toshiaki Seino <st12318@gmail.com>
2018-07-13 20:26:11 +0900

    user controllerを修正

--- app/serializers/v1/session_serializer.rb
+++ app/serializers/v1/session_serializer.rb
@@ -1,3 +1,13 @@
-class V1::SessionSerializer < ActiveModel::Serializer
-  attributes :id
+module V1
+  class SessionSerializer < ActiveModel::Serializer
+    attributes :email, :token_type, :user_id, :access_token
+
+    def user_id
+      object.id
+    end
+
+    def token_type
+      "Bearer"
+    end
+  end
 end
