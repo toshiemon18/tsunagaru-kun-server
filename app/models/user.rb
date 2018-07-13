13c679e250dae3bb0949be258e71c6337be5de26
Kenkichi Hattori <service@ahaufugyaha.com>
2018-07-13 21:49:29 +0900

    Userモデルのemailにvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
 
   after_create :update_access_token!
-  validates :email, presence: true
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
 
   def update_access_token!
     self.access_token = "#{self.id}:#{Devise.friendly_token}"
