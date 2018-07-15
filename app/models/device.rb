06580ae061c1723f15fcc560eff2e5dab1a2038f
望月建斗 <mochizuki@macsan.local>
2018-07-15 17:03:35 +0900

    deviceaモデルにvalidationを追加

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,7 @@
 class Device < ApplicationRecord
-    has_many :metrics
+  has_many :metrics
+  FORMAT_UTF8 = /[\u0000-\u007f]|[\u0080-\u07ff]|[\u0800-\uffff]|[\u{10000}-\u{1ffff}]/u
+  validates :name,
+            presence: true,
+            format: {with: FORMAT_UTF8}
 end
