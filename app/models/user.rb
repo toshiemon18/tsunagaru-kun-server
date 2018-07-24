51d742d528461a5c783c3cc0e494afb82b30f672
Toshiaki Seino <st12318@gmail.com>
2018-07-24 13:53:13 +0900

    DeviseTokeAuth::Concerns::Userをinclude

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+  include DeviseTokenAuth::Concerns::User
 
   after_create :update_access_token!
   validates :email, presence: true
