abbb6f18994e4de3ec307cd37a8a50b064a286c2
Toshiaki Seino <st12318@gmail.com>
2018-07-24 23:53:12 +0900

    devise_token_authでログイン等を制御

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   devise_for :users, only: []
   namespace :v1, defaults: {format: :json} do
     resource :login, only: [:create], controller: :sessions
