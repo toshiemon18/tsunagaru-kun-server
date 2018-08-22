bd4bb5230240df6d93278a2ec93280b9c56f5841
Toshiaki Seino <st12318@gmail.com>
2018-08-23 01:52:14 +0900

    ログインAPIの修正 (#21)
    * ログインAPIの修正
    
    * devise-token-authをアップデート
    
    * SessionsControllerを修正
    
    * ルーティングを修正
    
    * Userモデルを修正

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,33 +1,5 @@
 module V1
-  class SessionsController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:create]
+  class SessionsController < DeviseTokenAuth::SessionsController
 
-    # POST /v1/login
-    def create
-      @user = User.find_for_database_authentication(email: params[:email])
-      return invalid_email unless @user
-      if @user.valid_password?(params[:password])
-        sign_in :user, @user
-        render json: @user, serializer: V1::SessionSerializer, root: nil
-      else
-        invalid_password
-      end
-    end
-
-    private
-
-    def invalid_email
-      warden.custom_failure!
-      render json: { error: "Invalid email" }
-    end
-
-    def invalid_password
-      warden.custom_failure!
-      render json: { error: "Invalid password" }
-    end
-
-    def session_params
-      params.require(:user).permit(:email, :password)
-    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :devices, dependent: :destroy
+  before_create :generate_access_token
 
-  after_create :update_access_token!
+  has_many :devices, dependent: :destroy
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
             presence: true,
             uniqueness: true,
             format: { with: VALID_EMAIL_REGEX }
+  validates :encrypted_password, presence: true
+  # validates :access_token, presence: true, uniqueness: true
 
-  validates :password,
-            presence: true,
-            length: {minimum: 8},
-            format: {with: /\A[\w-]+\z/}
-
-  def update_access_token!
-    self.access_token = "#{self.id}:#{Devise.friendly_token}"
-    save
+  private
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
   end
 end
