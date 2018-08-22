3f7fbee1bd0794d66e0fa6f04f00b9f91ade5296
Toshiaki Seino <st12318@gmail.com>
2018-08-23 01:47:41 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, only: []
   namespace :v1, defaults: {format: :json} do
     mount_devise_token_auth_for 'User', at: 'auth',
-                                controllers: {registrations: 'v1/users'}
+      controllers: {registrations: 'v1/users',
+                    sessions: 'v1/sessions'}
     resources :devices
-    resources :users, only: [:create]
   end
 end
