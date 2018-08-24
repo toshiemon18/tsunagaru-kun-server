7a6cb692405b90195e4dab36739d684866cce6b3
Toshiaki Seino <st12318@gmail.com>
2018-08-24 23:24:00 +0900

    MetricsControllerのスケルトンを作成

--- app/controllers/v1/metrics_controller.rb
+++ app/controllers/v1/metrics_controller.rb
@@ -3,12 +3,21 @@ class V1::MetricsController < ApplicationController
 
   # GET /metricses
   def index
-    @metrices = Metrics.all
   end
 
   def show
   end
 
   def create
+  end
+
+  private
+  # for create method
+  def metrics_params
+  end
+
+  # for show method
+  def show_metrics_params
+    params.require(:metrics).permit(:device_id, :start_at, :end_at)
   end
 end
