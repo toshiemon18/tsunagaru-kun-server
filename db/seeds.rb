7d94bda33d9742c5c263ca7474ee5559cb365217
Kenkichi Hattori <service@ahaufugyaha.com>
2018-08-25 21:04:12 +0900

    seeds.rbからユーザを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(:name => 'kiyoshi', :email => 'kiyoshi@takaomi.com', :password => 'kiyoshi1234')
+user.save!
