960497dcb1a1bda40806db385c6b27b5b6c3234c
Toshiaki Seino <st12318@gmail.com>
2018-09-02 17:15:28 +0900

    after_createのcallbackを削除

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,11 +1,3 @@
 class Metric < ApplicationRecord
   belongs_to :device
-
-  after_create :set_other_metrics
-
-  private
-  def set_other_metrics
-    include MetricsEvaluator
-    self.watt = watt(self.electric_current)
-  end
 end
