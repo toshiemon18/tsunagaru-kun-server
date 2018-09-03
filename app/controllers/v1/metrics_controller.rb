28b507010a09d9001aa37e1af2806c796cae17d8
Toshiaki Seino <st12318@gmail.com>
2018-09-04 01:41:51 +0900

    metrics#showを実装

--- app/controllers/v1/metrics_controller.rb
+++ app/controllers/v1/metrics_controller.rb
@@ -1,5 +1,5 @@
 class V1::MetricsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:create, :show]
 
   def create
     @metrics = Metric.new(metrics_params)
@@ -11,9 +11,14 @@ class V1::MetricsController < ApplicationController
     end
   end
 
+  def show
+    @metrics = Metric.where(user_id: current_user.id).where(created_at: (6.month.ago)..Time.now)
+    render json: @metrics, serializer: V1::MetricsSerializer
+  end
+
   private
   # for create method
   def metrics_params
-    params.require(:metrics).permit(:electric_current, :watt, :device_id, :recording_time)
+    params.require(:metrics).permit(:electric_current, :watt, :device_id).merge(user_id: current_user.id)
   end
 end
