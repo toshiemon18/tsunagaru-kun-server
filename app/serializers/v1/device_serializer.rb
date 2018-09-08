f978f039e164585f6f5bbef4adab660cc2cd0f68
Toshiaki Seino <st12318@gmail.com>
2018-09-08 16:24:34 +0900

    関連データをserialize出来るように変更

--- app/serializers/v1/device_serializer.rb
+++ app/serializers/v1/device_serializer.rb
@@ -1,3 +1,11 @@
 class V1::DeviceSerializer < ActiveModel::Serializer
+  # device attributes
   attributes :id, :name, :category, :image
+
+  # device associations
+  has_many :half_year_metrics, serializer: MetricsSerializer
+
+  def half_year_metrics
+    object.metrics.half_year_records
+  end
 end
