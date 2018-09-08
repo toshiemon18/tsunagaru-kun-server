c2c45b030651e335fca94d2bdd8353b051373db2
Toshiaki Seino <st12318@gmail.com>
2018-09-08 16:23:53 +0900

    半年分のレコードを取り出すメソッドを追加

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -4,4 +4,8 @@ class Metric < ApplicationRecord
   scope :mine, -> (user_id) { where(user_id: user_id) }
   scope :order_by_created_at_desc, -> { order("created_at desc") }
   scope :half_year, -> { where(created_at: (6.month.ago)..Time.now) }
+
+  def half_year_records
+    self.half_year.order_by_created_at_desc
+  end
 end
