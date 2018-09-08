7c1f5511827c40b6ab4f93fde092c553f9a489f5
Toshiaki Seino <st12318@gmail.com>
2018-09-08 16:24:57 +0900

    reference名を修正

--- db/migrate/20180905095837_add_references_to_metrics.rb
+++ db/migrate/20180905095837_add_references_to_metrics.rb
@@ -1,8 +1,8 @@
 class AddReferencesToMetrics < ActiveRecord::Migration[5.1]
   def change
     change_table(:metrics) do |t|
-      t.references :users, index: true
-      t.references :devices, index: true
+      t.references :user, index: true
+      t.references :device, index: true
     end
   end
 end
