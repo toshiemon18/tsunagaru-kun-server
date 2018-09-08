2bf06e3040b58532fe667fa9c67a0a5a66181124
Toshiaki Seino <st12318@gmail.com>
2018-09-08 23:35:39 +0900

    名前空間を修正

--- app/serializers/v1/device_serializer.rb
+++ app/serializers/v1/device_serializer.rb
@@ -3,7 +3,7 @@ class V1::DeviceSerializer < ActiveModel::Serializer
   attributes :id, :name, :category, :image
 
   # device associations
-  has_many :half_year_metrics, serializer: MetricsSerializer
+  has_many :half_year_metrics, serializer: V1::MetricsSerializer
 
   def half_year_metrics
     object.metrics.half_year_records
