136810510407bedac3d1869ec5bad27992f90872
Takuya Mukohira <takuya.mk96@gmail.com>
2018-09-11 14:57:32 +0900

    Add tokens to sample user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# InsomniaやiOSアプリ等で、HTTPヘッダに
+#   uid: kiyoshi@takaomi.com
+#   client: sample
+#   access-token: sampletoken
+# をセットするとkiyoshiで認証される
 
-User.create(name: 'kiyoshi', email: 'kiyoshi@takaomi.com', password: 'kiyoshi1234')
+unless User.exists?(name: 'kiyoshi')
+  user = User.create(name: 'kiyoshi', email: 'kiyoshi@takaomi.com', password: 'kiyoshi1234')
+  user.tokens = {
+    sample: {
+      token: BCrypt::Password.create('sampletoken'),
+      expiry: (Time.now + 1.year).to_i
+    }
+  }
+end
