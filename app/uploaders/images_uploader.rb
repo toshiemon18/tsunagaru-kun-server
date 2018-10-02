be4997d17e601bcd6142d77bc807cae1d301e0bc
Kenkichi Hattori <service@ahaufugyaha.com>
2018-10-02 12:35:45 +0900

    アップローダーを作成

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -2,6 +2,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -21,17 +22,26 @@ class ImagesUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  process :convert => 'png'
+  process :tags => ['device_picture']
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
 
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+  version :thumbnail do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
